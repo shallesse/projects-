c227610d01d267e347abefc7157d8d2d275cd105
shallesse <39095506+shallesse@users.noreply.github.com>
2018-05-09 11:59:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,213 @@
+﻿<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Burger Joint</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <!--        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
+
+
+        <!--For Plugins external css-->
+        <link rel="stylesheet" href="assets/css/animate/animate.css" />
+        <link rel="stylesheet" href="assets/css/plugins.css" />
+
+        <!--Theme custom css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <!--Theme Responsive css-->
+        <link rel="stylesheet" href="assets/css/responsive.css" />
+
+        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		<div class='preloader'><div class='loaded'>&nbsp;</div></div>
+        <header id="home" class="navbar-fixed-top">
+            <div class="header_top_menu clearfix">	
+                <div class="container">
+                    <div class="row">	
+                        <div class="col-md-5 col-md-offset-3 col-sm-12 text-right">
+                            <div class="call_us_text">
+								<a href=""><i class="fa fa-clock-o"></i> Order Foods 24/7</a>
+								<a href=""><i class="fa fa-phone"></i>0730720026</a>
+							</div>
+                        </div>
+
+                        <div class="col-md-4 col-sm-12">
+                            <div class="head_top_social text-right">
+                                <a href=""><i class="fa fa-facebook"></i></a>
+                                <a href=""><i class="fa fa-google-plus"></i></a>
+                                <a href=""><i class="fa fa-twitter"></i></a>
+                              
+                            </div>	
+                        </div>
+
+                    </div>			
+                </div>
+            </div>
+
+            <!-- End navbar-collapse-->
+
+            <div class="main_menu_bg">
+                <div class="container"> 
+                    <div class="row">
+                        <nav class="navbar navbar-default">
+                            <div class="container-fluid">
+                                <!-- Brand and toggle get grouped for better mobile display -->
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                    <a class="navbar-brand our_logo" href="#"><img src="assets/images/logos.jpg"height="200" width="200" alt="" /></a>
+                                </div>
+
+                                <!-- Collect the nav links, forms, and other content for toggling -->
+                                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                                    <ul class="nav navbar-nav navbar-right">
+                                        <li><a href="Login/MenuUpdated1.php">Menu</a></li>
+                                         <li><a href="#">About</a></li>
+                                          <li><a href="login/Contact.php">Contact</a></li>
+                                          <li><a href="login/index.php">Login</a></li>
+                                           <li><a href="Contactform.php">Email </a></li>
+                                          
+                                       
+                                    </ul>
+                                </div><!-- /.navbar-collapse -->
+                            </div><!-- /.container-fluid -->
+                        </nav>
+                    </div>
+                </div>
+            </div>	
+        </header> <!-- End Header Section -->
+       
+        <p>
+        <!---Carosel-->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+                <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+                <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+
+            </ol>
+
+            <center>
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="assets/images/b3.jpg"  alt="Chania" />
+                    </div>
+
+                    <div class="item">
+                        <img src="assets/images/b2.jpg"  alt="Chania">
+                    </div>
+                    <div class="item">
+                        <img src="assets/images/b4.jpg" alt="Chania">
+                    </div>
+                    <div class="item">
+                        <img src="assets/images/b5.jpg" alt="Chania">
+                    </div>
+
+                    <div class="item">
+                        <img src="assets/images/b4.jpg" alt="Chania">
+                    </div>
+
+
+                </div>
+            </center>
+
+        </div>
+            </p>
+       
+        <section id="footer_widget" class="footer_widget">
+            <div class="container">
+                <div class="row">
+                    <div class="footer_widget_content text-center">
+                        <div class="col-md-4">
+                            <div class="single_widget wow fadeIn" data-wow-duration="2s">
+                                <h3>Find us at</h3>
+
+                                <div class="single_widget_info">
+                                    <p>112-Legere ancillae vix ne.
+
+                                        <span>Te elit putent propriae eum,</span>
+                                        <span>aliquip nominati</span>
+                                        <span class="phone_email">phone: 00 000 000</span>
+                                        <span>Email: support@templatemela.com</span></p>
+                                </div>
+
+                                <div class="footer_socail_icon">
+                                    <a href=""><i class="fa fa-facebook"></i></a>
+                                    <a href=""><i class="fa fa-google-plus"></i></a>
+                                    <a href=""><i class="fa fa-twitter"></i></a>
+                                    <a href=""><i class="fa fa-linkedin"></i></a>
+                                    <a href=""><i class="fa fa-pinterest-p"></i></a>
+                             
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="single_widget wow fadeIn" data-wow-duration="4s">
+                                <h3>Operating Hours</h3>
+
+                                <div class="single_widget_info">
+                                    <p><span class="date_day">Monday To Friday</span>
+                                        <span>8:00am to 10:00pm()</span>
+                                        
+
+                                        <span class="date_day">Saturday & Sunday</span>
+                                        <span>10:00am to 11:00pm(Brunch)</span>
+                                        <span>11:00am to 12:00pm(Lunch/Dinner)</span></p>
+                                </div>
+                            </div>
+                        </div>
+
+        </section>
+
+
+
+        <!--Footer-->
+        <footer id="footer" class="footer">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-sm-12">
+                        
+							
+						</div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+		
+		<div class="scrollup">
+			<a href="#"><i class="fa fa-chevron-up"></i></a>
+		</div>		
+
+
+        <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="assets/js/vendor/bootstrap.min.js"></script>
+
+        <script src="assets/js/jquery-easing/jquery.easing.1.3.js"></script>
+        <script src="assets/js/wow/wow.min.js"></script>
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
---
+++ send_mail.php
@@ -0,0 +1,35 @@
+<?php
+    $mailto = $_POST['email'];
+    $mailSub = $_POST['first_name'];
+    $mailMsg = $_POST['comments'];
+   require 'PHPMailer-master/PHPMailerAutoload.php';
+   $mail = new PHPMailer();
+   $mail ->IsSmtp();
+   $mail ->SMTPDebug = 0;
+   $mail ->SMTPAuth = true;
+   $mail ->SMTPSecure = 'ssl';
+   $mail ->Host = "smtp.gmail.com";
+   $mail ->Port = 465; // or 587
+   $mail ->IsHTML(true);
+   $mail ->Username = "yovaanyesteel707@gmail.com";
+   $mail ->Password = "yovaan1738*Y";
+   $mail ->SetFrom("yovaanyesteel707@gmail.com");
+   $mail ->Subject = $mailSub;
+   $mail ->Body = $mailMsg;
+   $mail ->AddAddress($mailto);
+
+   if(!$mail->Send())
+   {
+       echo "Mail Not Sent";
+   }
+   else
+   {
+       echo "Mail Sent";
+   }
+
+
+
+?>
+
+   
+
